fca5ac6cc5ebcf2bef44da87c4bb588030386ae8
Chris Hales <chris.hales.github@gmail.com>
2016-04-29 13:41:08 -0600

    batch.bash

--- batch.bash
+++ batch.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Chris Hales, 18 February 2016
+# Chris Hales, 29 April 2016
 # For use on NRAO Socorro cluster
 # May be useful elsewhere
 
@@ -9,6 +9,7 @@
 MYUSERNAME=chales
 NUM_JOBS=3          # set number of jobs you need processed, regulate behaviour using $JOB_ID in WRAPPER_SCRIPT
 MAX_PROCESSES=32    # recommended value 32. Note: Socorro cluster currently has 16 cores per node
+MEMORY_LIMIT=3800mb # memory to reserve per process
 MAIL_OPTIONS=a      # when do you want mail? a=abort, b=beginning, e=exit
 WAIT_TIME=120s      # interval to check system for progress of batch processing
 WORK_DIR=/PathToMyFiles
@@ -31,14 +32,15 @@ while [ $i -le $NUM_JOBS ]; do
     # don't submit more jobs than there are available cores
     let k=$i-$NUM_FINISHED
     while [ $k -gt $MAX_PROCESSES ]; do
-	sleep $WAIT_TIME
+        sleep $WAIT_TIME
         NUM_FINISHED=`ls -l $WORK_DIR/$WRAPPER_SCRIPT.o* 2> /dev/null | wc -l`
-	let k=$i-$NUM_FINISHED
+        let k=$i-$NUM_FINISHED
     done
     
     echo "job $i/$NUM_JOBS starting"
     # send each job to a single core (ppn=1) on a single node (nodes=1)
-    /opt/services/torque/bin/qsub -l nodes=1:ppn=1 -V -d $WORK_DIR -m $MAIL_OPTIONS $WORK_DIR/$WRAPPER_SCRIPT
+    /opt/services/torque/bin/qsub -l nodes=1:ppn=1 -l mem=$MEMORY_LIMIT,pmem=$MEMORY_LIMIT -V \
+                                  -d $WORK_DIR -m $MAIL_OPTIONS $WORK_DIR/$WRAPPER_SCRIPT
     
     let i+=1
 done
