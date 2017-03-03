a2911797ae71a1f66d6ad27773ca98f082a817c7
Chris Hales <chris.hales.github@gmail.com>
2017-03-03 10:10:15 +0000

    wrapper

--- wrapper.bash
+++ wrapper.bash
@@ -1,11 +1,12 @@
 #!/bin/bash
-# Chris Hales, 18 February 2016
+# Chris Hales, 3 March 2017
 # This script gets called from batch.bash
 # Place this script in your work directory as defined in batch.bash
 
 mkdir $WORK_DIR/testing_$JOB_ID
 cd $WORK_DIR/testing_$JOB_ID
 
-xvfb-run casa --nogui --nologger -c $WORK_DIR/$CASA_SCRIPT
+# -a will auto select virtual frame buffer instead of default 99
+xvfb-run -a casa --nogui --nologger -c $WORK_DIR/$CASA_SCRIPT
 
 exit 0
